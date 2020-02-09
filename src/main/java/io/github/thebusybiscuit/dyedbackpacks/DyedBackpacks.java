e43b55d9e153152e0d9d4f297a3d71199cee9a36
TheBusyBiscuit <mrCookieSlime@gmail.com>
2020-02-09 11:26:55 +0100

    Updated to latest Slimefun build

--- src/main/java/io/github/thebusybiscuit/dyedbackpacks/DyedBackpacks.java
+++ src/main/java/io/github/thebusybiscuit/dyedbackpacks/DyedBackpacks.java
@@ -1,17 +1,16 @@
 package io.github.thebusybiscuit.dyedbackpacks;
 
-import java.util.logging.Level;
-
+import org.bukkit.NamespacedKey;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import me.mrCookieSlime.CSCoreLibPlugin.general.Inventory.Item.CustomItem;
-import me.mrCookieSlime.CSCoreLibPlugin.general.World.CustomSkull;
 import me.mrCookieSlime.Slimefun.Lists.SlimefunItems;
 import me.mrCookieSlime.Slimefun.Objects.Category;
 import me.mrCookieSlime.Slimefun.Objects.Research;
 import me.mrCookieSlime.Slimefun.api.SlimefunItemStack;
 import me.mrCookieSlime.Slimefun.bstats.bukkit.Metrics;
 import me.mrCookieSlime.Slimefun.cscorelib2.config.Config;
+import me.mrCookieSlime.Slimefun.cscorelib2.skull.SkullItem;
 import me.mrCookieSlime.Slimefun.cscorelib2.updater.GitHubBuildsUpdater;
 
 public class DyedBackpacks extends JavaPlugin {
@@ -19,33 +18,28 @@ public class DyedBackpacks extends JavaPlugin {
 	@Override
 	public void onEnable() {
 		Config cfg = new Config(this);
-		new Metrics(this);
+		new Metrics(this, 5778);
 		
 		if (cfg.getBoolean("options.auto-update") && getDescription().getVersion().startsWith("DEV - ")) {
 			new GitHubBuildsUpdater(this, getFile(), "TheBusyBiscuit/DyedBackpacks/master").start();
 		}
 		
-		Research research = new Research(17200, "Dyed Backpacks", 24);
+		Research research = new Research(new NamespacedKey(this, "dyed_backpacks"), 17200, "Dyed Backpacks", 24);
 		
-		try {
-			Category category = new Category(new CustomItem(CustomSkull.getItem(BackpackColor.RED.getTexture()), "&4Dyed Backpacks", "", "&a> Click to open"));
-			
-			if (cfg.getBoolean("backpacks.small")) createBackpacks(category, research, (SlimefunItemStack) SlimefunItems.BACKPACK_SMALL, 9);
-			if (cfg.getBoolean("backpacks.normal")) createBackpacks(category, research, (SlimefunItemStack) SlimefunItems.BACKPACK_MEDIUM, 18);
-			if (cfg.getBoolean("backpacks.large")) createBackpacks(category, research, (SlimefunItemStack) SlimefunItems.BACKPACK_LARGE, 27);
-			if (cfg.getBoolean("backpacks.woven")) createBackpacks(category, research, (SlimefunItemStack) SlimefunItems.WOVEN_BACKPACK, 36);
-			if (cfg.getBoolean("backpacks.gilded")) createBackpacks(category, research, (SlimefunItemStack) SlimefunItems.GILDED_BACKPACK, 45);
-			if (cfg.getBoolean("backpacks.radiant")) createBackpacks(category, research, (SlimefunItemStack) SlimefunItems.RADIANT_BACKPACK, 54);
-			
-		} catch (Exception x) {
-			getLogger().log(Level.SEVERE, "An Exception occured while enabling the Plugin DyedBackpacks v" + getDescription().getVersion(), x);
-		}
+		Category category = new Category(new NamespacedKey(this, "dyed_backpacks"), new CustomItem(SkullItem.fromBase64(BackpackColor.RED.getTexture()), "&4Dyed Backpacks"));
+		
+		if (cfg.getBoolean("backpacks.small")) createBackpacks(category, research, (SlimefunItemStack) SlimefunItems.BACKPACK_SMALL, 9);
+		if (cfg.getBoolean("backpacks.normal")) createBackpacks(category, research, (SlimefunItemStack) SlimefunItems.BACKPACK_MEDIUM, 18);
+		if (cfg.getBoolean("backpacks.large")) createBackpacks(category, research, (SlimefunItemStack) SlimefunItems.BACKPACK_LARGE, 27);
+		if (cfg.getBoolean("backpacks.woven")) createBackpacks(category, research, (SlimefunItemStack) SlimefunItems.WOVEN_BACKPACK, 36);
+		if (cfg.getBoolean("backpacks.gilded")) createBackpacks(category, research, (SlimefunItemStack) SlimefunItems.GILDED_BACKPACK, 45);
+		if (cfg.getBoolean("backpacks.radiant")) createBackpacks(category, research, (SlimefunItemStack) SlimefunItems.RADIANT_BACKPACK, 54);
 		
 		research.register();
 	}
 
 	private void createBackpacks(Category category, Research research, SlimefunItemStack backpack, int size) {
-		for (BackpackColor color: BackpackColor.values()) {
+		for (BackpackColor color : BackpackColor.values()) {
 			SlimefunItemStack item = new SlimefunItemStack("DYED_" + backpack.getItemID() + "_" + color.name(), color.getTexture(), backpack.getItemMeta().getDisplayName() + " &7(" + color.getName() + "&7)", backpack.getItemMeta().getLore().toArray(new String[0]));
 			DyedBackpack dyed = new DyedBackpack(size, category, item, backpack, color);
 			research.addItems(dyed);
