99945ff4ea07ab6791cea57806b27b7b06f3102b
TheBusyBiscuit <mrCookieSlime@gmail.com>
2020-03-06 11:51:10 +0100

    Updated to latest Slimefun build

--- src/main/java/io/github/thebusybiscuit/dyedbackpacks/DyedBackpack.java
+++ src/main/java/io/github/thebusybiscuit/dyedbackpacks/DyedBackpack.java
@@ -3,26 +3,22 @@ package io.github.thebusybiscuit.dyedbackpacks;
 import org.bukkit.Material;
 import org.bukkit.inventory.ItemStack;
 
+import io.github.thebusybiscuit.slimefun4.implementation.items.tools.SlimefunBackpack;
 import me.mrCookieSlime.Slimefun.Lists.RecipeType;
 import me.mrCookieSlime.Slimefun.Objects.Category;
-import me.mrCookieSlime.Slimefun.Objects.SlimefunItem.SlimefunBackpack;
 import me.mrCookieSlime.Slimefun.api.SlimefunItemStack;
 import me.mrCookieSlime.Slimefun.cscorelib2.materials.MaterialCollections;
 
 public class DyedBackpack extends SlimefunBackpack {
 
-	public DyedBackpack(int size, Category category, SlimefunItemStack item, SlimefunItemStack backpack, BackpackColor color) {
-		super(size, category, item, RecipeType.ENHANCED_CRAFTING_TABLE, createRecipe(backpack, color));
-	}
+    public DyedBackpack(int size, Category category, SlimefunItemStack item, SlimefunItemStack backpack, BackpackColor color) {
+        super(size, category, item, RecipeType.ENHANCED_CRAFTING_TABLE, createRecipe(backpack, color));
+    }
 
-	private static ItemStack[] createRecipe(SlimefunItemStack backpack, BackpackColor color) {
-		ItemStack wool = new ItemStack(MaterialCollections.getAllWoolColors().get(color.ordinal()));
-		
-		return new ItemStack[] {
-			wool, wool, wool,
-			new ItemStack(Material.LEATHER), backpack, new ItemStack(Material.LEATHER),
-			wool, wool, wool
-		};
-	}
+    private static ItemStack[] createRecipe(SlimefunItemStack backpack, BackpackColor color) {
+        ItemStack wool = new ItemStack(MaterialCollections.getAllWoolColors().get(color.ordinal()));
+
+        return new ItemStack[] { wool, wool, wool, new ItemStack(Material.LEATHER), backpack, new ItemStack(Material.LEATHER), wool, wool, wool };
+    }
 
 }
--- src/main/java/io/github/thebusybiscuit/dyedbackpacks/DyedBackpacks.java
+++ src/main/java/io/github/thebusybiscuit/dyedbackpacks/DyedBackpacks.java
@@ -15,46 +15,46 @@ import me.mrCookieSlime.Slimefun.cscorelib2.skull.SkullItem;
 import me.mrCookieSlime.Slimefun.cscorelib2.updater.GitHubBuildsUpdater;
 
 public class DyedBackpacks extends JavaPlugin implements SlimefunAddon {
-	
-	@Override
-	public void onEnable() {
-		Config cfg = new Config(this);
-		new Metrics(this, 5778);
-		
-		if (cfg.getBoolean("options.auto-update") && getDescription().getVersion().startsWith("DEV - ")) {
-			new GitHubBuildsUpdater(this, getFile(), "TheBusyBiscuit/DyedBackpacks/master").start();
-		}
-		
-		Research research = new Research(new NamespacedKey(this, "dyed_backpacks"), 17200, "Dyed Backpacks", 24);
-		
-		Category category = new Category(new NamespacedKey(this, "dyed_backpacks"), new CustomItem(SkullItem.fromBase64(BackpackColor.RED.getTexture()), "&4Dyed Backpacks"));
-		
-		if (cfg.getBoolean("backpacks.small")) createBackpacks(category, research, (SlimefunItemStack) SlimefunItems.BACKPACK_SMALL, 9);
-		if (cfg.getBoolean("backpacks.normal")) createBackpacks(category, research, (SlimefunItemStack) SlimefunItems.BACKPACK_MEDIUM, 18);
-		if (cfg.getBoolean("backpacks.large")) createBackpacks(category, research, (SlimefunItemStack) SlimefunItems.BACKPACK_LARGE, 27);
-		if (cfg.getBoolean("backpacks.woven")) createBackpacks(category, research, (SlimefunItemStack) SlimefunItems.WOVEN_BACKPACK, 36);
-		if (cfg.getBoolean("backpacks.gilded")) createBackpacks(category, research, (SlimefunItemStack) SlimefunItems.GILDED_BACKPACK, 45);
-		if (cfg.getBoolean("backpacks.radiant")) createBackpacks(category, research, (SlimefunItemStack) SlimefunItems.RADIANT_BACKPACK, 54);
-		
-		research.register();
-	}
 
-	private void createBackpacks(Category category, Research research, SlimefunItemStack backpack, int size) {
-		for (BackpackColor color : BackpackColor.values()) {
-			SlimefunItemStack item = new SlimefunItemStack("DYED_" + backpack.getItemID() + "_" + color.name(), color.getTexture(), backpack.getItemMeta().getDisplayName() + " &7(" + color.getName() + "&7)", backpack.getItemMeta().getLore().toArray(new String[0]));
-			DyedBackpack dyed = new DyedBackpack(size, category, item, backpack, color);
-			research.addItems(dyed);
-			dyed.register(this);
-		}
-	}
+    @Override
+    public void onEnable() {
+        Config cfg = new Config(this);
+        new Metrics(this, 5778);
 
-	@Override
-	public String getBugTrackerURL() {
-		return "https://github.com/TheBusyBiscuit/DyedBackpacks/issues";
-	}
+        if (cfg.getBoolean("options.auto-update") && getDescription().getVersion().startsWith("DEV - ")) {
+            new GitHubBuildsUpdater(this, getFile(), "TheBusyBiscuit/DyedBackpacks/master").start();
+        }
 
-	@Override
-	public JavaPlugin getJavaPlugin() {
-		return this;
-	}
+        Research research = new Research(new NamespacedKey(this, "dyed_backpacks"), 17200, "Dyed Backpacks", 24);
+
+        Category category = new Category(new NamespacedKey(this, "dyed_backpacks"), new CustomItem(SkullItem.fromBase64(BackpackColor.RED.getTexture()), "&4Dyed Backpacks"));
+
+        if (cfg.getBoolean("backpacks.small")) createBackpacks(category, research, (SlimefunItemStack) SlimefunItems.BACKPACK_SMALL, 9);
+        if (cfg.getBoolean("backpacks.normal")) createBackpacks(category, research, (SlimefunItemStack) SlimefunItems.BACKPACK_MEDIUM, 18);
+        if (cfg.getBoolean("backpacks.large")) createBackpacks(category, research, (SlimefunItemStack) SlimefunItems.BACKPACK_LARGE, 27);
+        if (cfg.getBoolean("backpacks.woven")) createBackpacks(category, research, (SlimefunItemStack) SlimefunItems.WOVEN_BACKPACK, 36);
+        if (cfg.getBoolean("backpacks.gilded")) createBackpacks(category, research, (SlimefunItemStack) SlimefunItems.GILDED_BACKPACK, 45);
+        if (cfg.getBoolean("backpacks.radiant")) createBackpacks(category, research, (SlimefunItemStack) SlimefunItems.RADIANT_BACKPACK, 54);
+
+        research.register();
+    }
+
+    private void createBackpacks(Category category, Research research, SlimefunItemStack backpack, int size) {
+        for (BackpackColor color : BackpackColor.values()) {
+            SlimefunItemStack item = new SlimefunItemStack("DYED_" + backpack.getItemID() + "_" + color.name(), color.getTexture(), backpack.getItemMeta().getDisplayName() + " &7(" + color.getName() + "&7)", backpack.getItemMeta().getLore().toArray(new String[0]));
+            DyedBackpack dyed = new DyedBackpack(size, category, item, backpack, color);
+            research.addItems(dyed);
+            dyed.register(this);
+        }
+    }
+
+    @Override
+    public String getBugTrackerURL() {
+        return "https://github.com/TheBusyBiscuit/DyedBackpacks/issues";
+    }
+
+    @Override
+    public JavaPlugin getJavaPlugin() {
+        return this;
+    }
 }
